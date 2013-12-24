4c0fb651e35a575d1993797f3b575693cbfa8d56
Aseem Sood <asood123@yahoo.com>
2013-12-24 10:11:56 -0500

    minor tweaks to input system

--- src/com/game/loot/PhysicalPlayer.java
+++ src/com/game/loot/PhysicalPlayer.java
@@ -51,12 +51,13 @@ public class PhysicalPlayer extends Player {
 		}
 		
 		
-		System.out.println("What would you like to do? Ex: M5, P2, TA, Enter (to draw), etc.");
+
 		
 		while (!done){
 			try {
+				System.out.println("What would you like to do? Ex: M5, P2, TA, [Enter] (to draw), etc.");
 				String input = br.readLine();
-				
+
 				if (input.equalsIgnoreCase("d") || input.equalsIgnoreCase("draw")
 						|| input.equalsIgnoreCase("")) {
 					if (deckSize > 0) {
@@ -102,95 +103,9 @@ public class PhysicalPlayer extends Player {
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (Exception e) {
-				System.err.println("Input unclear. Try again");
+				System.err.println("Input unclear. Try again!");
 			}
 		}
-		
-		
-		
-		/*
-		System.out.println(ACTION.DRAW.ordinal() + ": Draw card");
-		System.out.println(ACTION.PLAY_MERCHANT_SHIP.ordinal() + ": Play merchant ship");
-		System.out.println(ACTION.PLAY_ATTACK.ordinal() + ": Play attack card");
-		System.out.println(ACTION.DISCARD.ordinal() + ": Discard card");
-		
-        try {
-            ACTION a = ACTION.values()[Integer.parseInt(br.readLine())];
-            
-            switch(a) {
-            case DRAW:
-            	return new Move(ACTION.DRAW, null, null);
-            case PLAY_MERCHANT_SHIP:
-            	Card playCard = null;
-            	while (playCard == null) {
-            		System.out.println("Which card?");
-                	String cardString = br.readLine();
-                	
-                	playCard = hand.findCardFromString(cardString);
-            	
-	            	if (playCard != null) {
-	            		if (playCard instanceof MerchantShip) {
-	            			return new Move(ACTION.PLAY_MERCHANT_SHIP, playCard, null);
-	            		} else {
-	            			System.out.println("Didn't find that ship, please try again");
-	            			playCard = null;
-	            		}
-	            	} else {
-	            		System.out.println("Didn't find that ship, please try again");
-	            	}
-            	}
-            	
-            	break;
-            case PLAY_ATTACK:
-            	playCard = null;
-            	while (playCard == null) {
-            		System.out.println("Which card?");
-                	String cardString = br.readLine();
-                	
-                	playCard = hand.findCardFromString(cardString);
-            	
-	            	if (playCard != null) {
-	            		if (playCard instanceof AttackCard) {
-	            			Battle battle = null;
-	            			while (battle == null) {
-	            				System.out.println("Which battle do you want to attack?");
-	            				int battleId = Integer.parseInt(br.readLine());
-	            				battle = gameState.findBattleById(battleId);
-	            				if (battle == null) {
-	            					System.out.println("That battle wasn't found, please enter another.");
-	            				}
-	            			}                  	
-	                    	
-	            			return new Move(ACTION.PLAY_ATTACK, playCard, battle);
-	            		} else {
-		            		System.out.println("Didn't find that ship, please try again");
-		            		playCard = null;
-	            		}
-	            	} else {
-	            		System.out.println("Didn't find that ship, please try again");
-	            	}
-            	}
-            case DISCARD:
-            	Card discardCard = null;
-            	while (discardCard == null) {
-            		System.out.println("Which card?");
-                	String cardString = br.readLine();
-                	
-                	discardCard = hand.findCardFromString(cardString);
-	            	if (discardCard != null) {
-	            		return new Move(ACTION.DISCARD, discardCard, null);
-	            	} else {
-	            		System.out.println("Didn't find that card, please try again");
-	            	}
-            	}
-            	break;
-            }
-        } catch(NumberFormatException nfe){
-            System.err.println("Invalid Format!");
-        } catch (IOException e) {
-			e.printStackTrace();
-		}
-		*/
 		return null;
 		
 	}
